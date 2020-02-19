f5b2d6e05124db6bedced606c83a803ff3e26631
invite-you <50567167+invite-you@users.noreply.github.com>
2020-02-19 21:29:03 +0900

    Update augmentation.py

--- datasets/augmentation.py
+++ datasets/augmentation.py
@@ -94,7 +94,7 @@ def collater(data):
 class Resizer(object):
     """Convert ndarrays in sample to Tensors."""
 
-    def __call__(self, sample, common_size=512):
+    def __call__(self, sample, common_size=1024):
         image, annots = sample['img'], sample['annot']
         height, width, _ = image.shape
         if height > width:
